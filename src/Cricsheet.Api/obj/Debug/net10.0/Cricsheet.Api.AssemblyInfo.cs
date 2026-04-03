babcad798b2d6724cd00a7d72d54ff87d82a9d00
David <d.a.baker246@gmail.com>
2026-04-03 23:15:08 +0100

    /speckit.tasks complete specs\001-ballbyball-read-api T028

--- src/Cricsheet.Api/obj/Debug/net10.0/Cricsheet.Api.AssemblyInfo.cs
+++ src/Cricsheet.Api/obj/Debug/net10.0/Cricsheet.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cricsheet.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc413c660b33cfa05fe0772774ac87d9eee3904c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16a0a0075f4fe22106e04a6d4dc98588a7ae261f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cricsheet.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cricsheet.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
